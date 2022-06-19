f1800d929bd8b9a46965859be67215e72ce4327e
yanj1337 <holzyt@mail.ru>
2022-06-19 15:39:43 +0300

    небольшие изменения

--- index.js
+++ index.js
@@ -48,6 +48,24 @@ const swiper = new Swiper(".swiper", {
   scrollbar: {
     el: ".swiper-scrollbar",
   },
+
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 });
 
 /*     //  Slider
